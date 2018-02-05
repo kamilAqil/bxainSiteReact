bf70d23d6a0d21dfdf5b12d6743e32445fe4aed5
Kamil <kaqil91@gmail.com>
2018-02-04 19:07:03 -0800

    going to start putting in monsy theme

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname,'../','bxain_front_end', 'build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../', 'bxain_front_end', 'build', 'index.html'));
 });
 
-app.listen(9000);
+
+
+app.listen(9000,()=>{
+    console.log('app listening on port 9000');
+});
