3af1a355f22df57c41220b8f52570ee151d4d499
Kamil <kaqil91@gmail.com>
2018-02-05 17:25:57 -0800

    broke stuff into components

--- server/server.js
+++ server/server.js
@@ -10,11 +10,8 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../', 'public', 'index.html'));
 });
 
-<<<<<<< HEAD
-app.listen(PORT, ()=>{
-    console.log('listening on port',PORT);
-=======
+
 app.listen(process.env.PORT || 8080,()=>{
     console.log('listening on port',8080);
->>>>>>> 4a11a0def9db2ec760e1e564c45319e1a4a04683
+
 });
